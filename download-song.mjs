408f77e93de171fae5eb7dcc3f2c8db9c5311eb5
DoctorCheese1 <71832931+DoctorCheese1@users.noreply.github.com>
2026-03-19 18:46:23 -0500

    Force non-shell child process execution in downloader

--- download-song.mjs
+++ download-song.mjs
@@ -23,9 +23,9 @@ function ask(question) {
 function run(command, args, options = {}) {
   return new Promise((resolve, reject) => {
     const child = spawn(command, args, {
-      shell: true,
       stdio: "inherit",
-      ...options
+      ...options,
+      shell: false
     });
 
     child.on("error", reject);
